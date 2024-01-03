27b89c52b281d1e391fba914f98bfbad91b81af1
Will <54237626+PhoenixWyllow@users.noreply.github.com>
2024-01-03 22:44:03 +0100

    add nvim files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,60 @@
+-- There are `:help X` comments which you should run to read the help section for more information.
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Setting options ]]
+require('baseoptions')
+
+-- [[ Basic Keymaps ]]
+require('keymaps')
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure plugins ]]
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup('plugins', {})
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- [[ Configure Telescope ]]
+require('conf_telescope')
+
+-- [[ Configure Treesitter ]]
+require('conf_treesitter')
+
+-- [[ Configure LSP ]]
+require('conf_lsp')
+
+-- [[ Configure nvim-cmp ]]
+require('conf_cmp')
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,17 @@
+local kmap = function(mode, keys, func, opts)
+  vim.keymap.set(mode, keys, func, opts)
+end
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+kmap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+kmap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+kmap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+kmap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+kmap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+kmap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+kmap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
